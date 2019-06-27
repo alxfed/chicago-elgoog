7dd26c49984d469fac4e00180d467c731d7a6d55
Alex Fedotov <alxfed@gmail.com>
2019-06-27 10:39:16 -0500

    grid csv and kml

--- cook_county_grid_csv.py
+++ cook_county_grid_csv.py
@@ -3,17 +3,17 @@ geo-grid for Maps API search
 '''
 import csv
 
-top = 42.16
-right = 87.51
+top = 42.152
+right = -87.51
 
-init_lat = 41.4
+init_lat = 41.46
 lat_step = .018 # 2000m
-init_lng = -88.3
+init_lng = -88.28
 lng_step = .024 # 2000m
 indx = 1
 
 lat = init_lat
-with open('chicago_grid.csv', 'w') as csvfile:
+with open('cook_county_grid.csv', 'w') as csvfile:
     gridwriter = csv.writer(csvfile, delimiter=',')
     while lat <= top:
         lng = init_lng
